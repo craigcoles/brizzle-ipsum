10c4cd545d12b44c0b480b016ef3f4691aae92c3
discoliam <discoliam@googlemail.com>
2013-02-26 12:26:25 +0000

    bit of a re-design

--- index.php
+++ index.php
@@ -58,35 +58,14 @@
   
   <!-- Favicon: -->
   <link rel="shortcut icon" href="/favicon.ico">
-  <!-- For iPhone 4 with high-resolution Retina display: -->
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/apple-touch-icon-114x114-precomposed.png">
-  <!-- For first-generation iPad: -->
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/apple-touch-icon-72x72-precomposed.png">
-  <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
   <link rel="apple-touch-icon-precomposed" href="assets/images/apple-touch-icon-precomposed.png">
-  
-  <!-- so:stylesheets -->
-  <!--[if ! lte IE 6]><!-->
-    <link href="assets/stylesheets/application.css" media="screen,tv,projection" rel="Stylesheet" type="text/css" />
-    <!--[if lte IE 9]><link href="assets/stylesheets/ie9.css" media="screen" rel="Stylesheet" type="text/css" /><![endif]-->
-    <!--[if lte IE 8]><link href="assets/stylesheets/ie8.css" media="screen" rel="Stylesheet" type="text/css" /><![endif]-->
-    <!--[if lte IE 7]><link href="assets/stylesheets/ie7.css" media="screen" rel="Stylesheet" type="text/css" /><![endif]-->
-  <!--<![endif]-->
-  <!--[if !IE 7]>
-    <style type="text/css">
-      .content {display:table;height:100%}
-    </style>
-  <![endif]-->
-  <!--[if lte IE 6]>
-    <link rel="stylesheet" href="http://universal-ie6-css.googlecode.com/files/ie6.1.1.css" media="screen, projection">
-  <![endif]-->
-  <!-- eo:stylesheets -->
 
+  <link href="assets/stylesheets/application.css" media="screen,tv,projection" rel="Stylesheet" type="text/css" />
+   
   <!-- so:header JS -->
   
-  <!--[if lt IE 9]>
-  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
   <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.0.6/modernizr.min.js"></script>
   
   <!-- Google Analytics -->
@@ -108,54 +87,50 @@
 
 <body>
 
-  <div classs="page">
+  <div class="content">
+    <h1>Bristol Ipsum</h1>
 
-    <div class="content">
+    <form id="luvver" class="block" action="" method="get">
 
-      <form id="luvver" class="block" action="" method="get">
+      <div class="form-wrap">
+        <label for="paragraphs">Num. of Paragraphs:</label>
+        <input id="paragraphs" style="width: 40px;" type="text" name="paras" value="5" maxlength="2" />
+      </div>
 
-        <div class="form-wrap">
-          <label for="paragraphs">Paragraphs:</label>
-          <input id="paragraphs" style="width: 40px;" type="text" name="paras" value="5" maxlength="2" />
-        </div>
+      <label for="paragraphs">Type of Ipusm:</label>
+      <ul>
+        <li>
+          <input id="luvver-and-filler" type="radio" name="type" value="luvver-and-filler" />
+          <label for="luvver-and-filler">Luvver and Filler</label>
+        </li> 
+        <li>
+          <input id="proper-gurt-lush" type="radio" name="type" value="proper-gurt-lush" />
+          <label for="proper-gurt-lush">Proper Gurt Lush</label> 
+        </li>
+      </ul>
+     
 
-        <div class="form-wrap">
-          <label for="paragraphs">Type:</label>
-            <ul>
-              <li>
-                <input id="luvver-and-filler" type="radio" name="type" value="luvver-and-filler" />
-                <label for="luvver-and-filler">Luvver and Filler</label>
-              </li> 
-              <li>
-                <input id="proper-gurt-lush" type="radio" name="type" value="proper-gurt-lush" />
-                <label for="proper-gurt-lush">Proper Gurt Lush</label> 
-              </li>
-            </ul>
-        </div>
+      <input id="start-with-luvver" type="checkbox" name="start-with-luvver" value="true" /> 
+      <label for="start-with-luvver">Start with 'All right me luvver...'</label>
 
-        <div class="form-wrap">
-          <input id="start-with-luvver" type="checkbox" name="start-with-luvver" value="true" /> 
-          <label for="start-with-luvver">Start with 'All right me luvver ipsum dolor sit amet...'</label>
-        </div>
+      <input id="generate" type="submit" value="Generate" />
 
-        <div class="form-wrap buttons">
-          <input id="generate" type="submit" value="Mint in it" />
-          <p class="copy-to-clipboard">Copy to clipboard</p>
-        </div>
+    </form>
 
-    	</form>
-
-    	<div class="block output <?php if (isset($_REQUEST["type"])) echo 'display'?>">
-        <?php if (isset($_REQUEST["type"])) { print $output; }  ?> 
-      </div> 
-      
+    <div class="block output">
+      <?php if (isset($_REQUEST["type"])) { print $output; }  ?> 
+      <p class="quote"><span class="mark">"</span>A dialect of English is spoken by some Bristol inhabitants, known colloquially as Bristolian, "Bristolese" or even, following the publication of Derek Robson's "Krek Waiters peak Bristle", as "Bristle" or "Brizzle". Bristol natives speak with a rhotic accent, in which the post-vocalic r in words like car and card is still pronounced, having been lost from many other dialects of English, notably BBC English, or RP, i.e., "received pronunciation". The unusual feature of this accent, unique to Bristol, is the so-called Bristol L (or terminal L), in which an L sound appears to be appended to words that end in an 'a' or 'o'. There is some dispute about whether this is broad "l" or "w".[210] Thus "area" becomes "areal" or "areaw", etc. The "-ol" ending of the city's name is a significant example of the occurrence of the so-called "Bristol L". Bristolians using the dialect, tend to pronounce "a" and "o" at the end of a word almost as "aw", hence "cinemaw". To the stranger's ear this pronunciation sounds as if there is an "L" after the vowel. For example the statement "Africa is a malaria area", spoken with a Bristolian accent will sound like "Africal is a malarial areal" or "Africaw is a malariaw areaw". Another example of the Bristol accent is broad a before "l", thus "Awbert" for "Albert", "fawt" for "fault".<span class="mark">"</span></p>
+      <p>From <a href="http://en.wikipedia.org/wiki/Bristol#Dialect" title="Bristol Dialect on Wikipedia">Wikiepdia</a>.</p>
     </div>
-    
-    <footer class="site-footer">
-      <p>Background Image: <a href="http://www.flickr.com/photos/danbri/8072271/">http://www.flickr.com/photos/danbri/8072271/</a></p>
-    </footer>
 
   </div>
+
+  <footer class="site-footer">
+  <p>Made 2013 <a href="http://craigcoles.co.uk" title="Craig Coles">Craig Coles</a>. With help from <a href="http://discoliam.com" title="DiscoLiam">Liam Richardson</a>.<br />Background Image: <a href="http://www.flickr.com/photos/danbri/8072271/">http://www.flickr.com/photos/danbri/8072271/</a>. Based on <a href="https://github.com/petenelson/bacon-ipsum" title="Bacon Ipsum on Github">Bacon Ipsum</a> by <a href="https://twitter.com/GunGeekATX" title="Pete Nelson on Twitter">Pete Nelson</a>.</p>
+  </footer>
+
+
+
 
   <!-- so:JavaScripts -->
   <!-- libraries  -->
